5f673e265ba536ab85b81d71f8afe6957dedeb86
abhozeetyadav <ajeetusamni@gmail.com>
2023-12-05 14:09:40 +0545

    First commit

---
+++ ecr.py
@@ -0,0 +1,11 @@
+import boto3
+region = 'ap-south-1'
+ecr_client = boto3.client('ecr',region_name=region)
+
+
+repo_name = "monitoring_app_image"
+response = ecr_client.create_repository(repositoryName=repo_name)
+
+repository_uri = response ['repository']['repositoryUri']
+print(repository_uri)
+
